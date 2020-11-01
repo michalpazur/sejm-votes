932f89e7a1631bdf462b0c6e6a1ad32fffbd2456
michalpazur <michal.pazur1@gmail.com>
2020-11-01 18:23:37 +0100

    Added sorting by average correlation when creating image

--- create_image.py
+++ create_image.py
@@ -2,16 +2,19 @@ import json, math
 from PIL import Image, ImageDraw
 from matplotlib.pylab import get_cmap
 
+def average(arr):
+    return sum(arr)/len(arr)
+
 with open('vote_results_2.json', 'r') as f:
     results = json.load(f)
 
 avgs = {}
 std_vars = {}
+corrs = {}
 length = len(results['PiS'])
 
 for key in results:
-    avg = sum(results[key]) / length
-    avgs[key] = avg
+    avgs[key] = average(results[key])
 
 for key in results:
     std_var = 0
@@ -22,11 +25,33 @@ for key in results:
 
 print(std_vars)
 
-img = Image.new('RGBA', (3000, 3000))
 x = 0
 for key_x in results:
     y = 0
     for key_y in results:
+        cov = 0
+        for i in range(length):
+            value = (results[key_x][i] - avgs[key_x]) * (results[key_y][i] - avgs[key_y])
+            cov += value
+        corr = cov / (std_vars[key_x] * std_vars[key_y])
+        print(key_x, key_y, corr)
+        arr = corrs.setdefault(key_x, [])
+        arr.append(corr)
+        corrs[key_x] = arr
+    x += 1
+
+print(corrs)
+sorted_results_keys = sorted(list(corrs.keys()), key=lambda key: average(corrs[key]))
+print(sorted_results_keys)
+sorted_results = {}
+for key in sorted_results_keys:
+    sorted_results[key] = results[key]
+
+img = Image.new('RGBA', (3000, 3000))
+x = 0
+for key_x in sorted_results:
+    y = 0
+    for key_y in sorted_results:
         cov = 0
         for i in range(length):
             value = (results[key_x][i] - avgs[key_x]) * (results[key_y][i] - avgs[key_y])
