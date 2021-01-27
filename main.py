4f4f23d2cff479847cbf5b7c6ec4ed83df9618e4
michalpazur <michal.pazur1@gmail.com>
2021-01-27 13:02:44 +0100

    Fixed issue with table parsing

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ vote_results = {
     'Konfederacja': [],
     'niez.': [],
 }
+
 party_names = {
     'PiS': 'PiS',
     'KO': 'KO',
     'Lewica': 'Lewica',
+    'KP': 'PSL-Kukiz15',
     'PSL-Kukiz15': 'PSL-Kukiz15',
     'Konfederacja': 'Konfederacja',
     'niez.': 'niez.',
@@ -47,43 +49,28 @@ for all_votes_link in all_days_soup.find('tbody').findAll('a'):
         
         print(vote_link.text)
         vote_soup = bs(requests.get(base_link + vote_link['href']).content, 'html.parser')
-        pis_results = [0, 0, 0, 0] #za, przeciw, wstrzymało się, nieobecni
-        pis_ratio = 0
-        row_index = 0
 
-        for party_row in vote_soup.findAll('tr'):
-            if (row_index == 0):
-                row_index += 1
-                continue
+        all_cells = vote_soup.find("tbody").findAll("td")
+
+        for i in range(6):
+            row = all_cells[i * 7:(i * 7) + 6]
             cell_index = 0
-            party_name = ''
-            party_results = [0, 0, 0, 0]
-            for cell in party_row.findAll('td'):
+
+            for cell in row:
+                party_name = ''
+                party_results = [0, 0, 0, 0]
                 if (cell_index == 0):
                     party_name = cell.text
-
-                cell_text = cell.text
-                if (cell_text == '-'):
-                    cell_text = '0'
-
-                if (cell_index > 2 and row_index == 1):
-                    pis_results[cell_index - 3] = int(cell_text)
                 elif (cell_index > 2):
+                    cell_text = cell.text
+                    if (cell_text == '-'):
+                        cell_text = '0'
+
                     party_results[cell_index - 3] = int(cell_text)
-                cell_index += 1
-            
-            if (row_index == 1):
-                pis_ratio = get_results(pis_results)
-                print(party_name, pis_ratio)
-                vote_results[party_names[party_name]].append(pis_ratio)
-            else:
-                party_ratio = get_results(party_results)
-                if (party_ratio != -2):
-                    print(party_name, 'niezgodnie z PiSem' if abs(party_ratio - pis_ratio) > 0.2 else 'zgodnie z PiSem', party_ratio)
-                else:
-                    print(party_name, 'nieobecni')
-                vote_results[party_names[party_name]].append(party_ratio)
-            row_index += 1
+                    cell_index += 1
+                
+                    party_ratio = get_results(party_results)
+                    vote_results[party_names[party_name]].append(party_ratio)
 
 with open('vote_results.json', 'w', encoding='utf-8') as f:
     json.dump(vote_results, f)
