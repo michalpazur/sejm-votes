e3d6354951177de44a5bb7f22e6f3c9f81b4ccf8
michalpazur <michal.pazur1@gmail.com>
2021-06-29 21:38:08 +0200

    Vote result are being saved to database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import backend.models as m
+import datetime
 
 def get_results(vote_results):
     all_votes = sum(vote_results)
@@ -20,6 +22,13 @@ vote_results_indicies = {
     "Przeciw": 1,
     "Wstrzymał się": 2,
     "Nieobecny": 3,
+}
+
+vote_results_values = {
+    "Za": 1,
+    "Przeciw": -1,
+    "Wstrzymał się": 0,
+    "Nieobecny": -2,
 }
 
 base_link = 'http://www.sejm.gov.pl/Sejm9.nsf/'
@@ -50,23 +59,63 @@ with open("vote_results.csv", "w") as f:
 for all_votes_link in all_days_soup.find('tbody').findAll('a'):
     print(all_votes_link.text)
     all_votes_soup = bs(requests.get(base_link + all_votes_link['href']).content, 'html.parser')
-    for vote_row in all_votes_soup.findAll('td', class_='left'):
+
+    header = all_votes_soup.find("h1").text.split(" ")
+    #Głosowania w dniu DD-MM-YYYY r. na XX. posiedzeniu Sejmu
+    date = header[3].split("-")
+    sitting_number = int(header[6][:-1])
+    sitting = None
+    try:
+        sitting = m.Sitting.get(m.Sitting.number == sitting_number)
+    except:
+        print("Creating sitting with number {}".format(sitting_number))
+        sitting = m.Sitting(number=sitting_number)
+        sitting.save()
+    
+    day = None
+    date_formatted = datetime.date(int(date[2]), int(date[1]), int(date[0]))
+    try:
+        day = m.Day.get(m.Day.date == date_formatted)
+    except:
+        print("Creating new day: {}".format(".".join(date)))
+        day = m.Day(date=date_formatted, sitting=sitting)
+        day.save()
+    
+    for row in all_votes_soup.find("tbody").findAll('tr'):
+        vote_row = row.find("td", class_="left")
         vote_link = vote_row.find('a')
         if ('stwierdzenie kworum' in vote_link.text or 'przerwy w obradach' in vote_link.text or 'odroczeniem posiedzenia' in vote_link.text):
             continue
 
-        with open("titles.txt", "a", encoding="utf-8") as f:
-            f.write(vote_row.text + "\n")
-        
-        print(vote_link.text)
+        title = vote_row.text
+        short_title = vote_link.text
+
+        vote = None
+        vote_number = int(row.find("td", class_="bold").text)
+        try:
+            vote = m.Vote.get((m.Vote.day == day) & (m.Vote.number == vote_number))
+        except:
+            print("Creating vote with number {}...".format(vote_number))
+            vote = m.Vote(day=day, title=title, number=vote_number)
+            vote.save()
+
+        #If all the votes have been counted there's no need to count them again
+        count = m.Result.select().where(m.Result.vote == vote).count()
+        if (count == 460):
+            print("Skipping vote {}, all votes are counted.".format(short_title))
+            continue
+
+        #If not, download the page and count the votes
         vote_soup = bs(requests.get(base_link + vote_link['href']).content, 'html.parser')
-        
         try:
             all_cells = vote_soup.find("tbody").findAll("td", "left")
         except:
-            print("!!!")
+            print("Encountered an error while parsing HTML.")
             continue
-        
+
+        with open("titles.txt", "a", encoding="utf-8") as f:
+            f.write(title + "\n")
+
         tmp_party_results = {k: [0, 0, 0, 0] for k in party_names}
         should_pass = True
 
@@ -81,13 +130,33 @@ for all_votes_link in all_days_soup.find('tbody').findAll('a'):
 
             #one MP has two cells - one for their name and one for their vote
             #hence the division by two
-            for i in range(int(len(all_results_cells)/2)): 
+            for i in range(int(len(all_results_cells)/2)):
                 name = all_results_cells[i * 2].text
-                result = vote_results_indicies[all_results_cells[(i * 2) + 1].text]
+                res = all_results_cells[(i * 2) + 1].text
+                result = vote_results_indicies[res]
+                result_value = vote_results_values[res]
                 party_name = mps[name]
                 arr = tmp_party_results[party_name]
                 arr[result] = arr[result] + 1
                 tmp_party_results[party_name] = arr
+
+                first_name, last_name = m.split_name(name)
+                deputy = None
+                try:
+                    deputy = m.Deputy.get((m.Deputy.first_name == first_name) & (m.Deputy.last_name == last_name))
+                except:
+                    print("No deputy with name {} {} found.".format(first_name, last_name))
+                    print("Perhaps you forgot to run mps.py first?")
+                    quit(-1)
+
+                vote_result = None
+                try:
+                    vote_result = m.Result.get((m.Result.deputy == deputy) & (m.Result.vote == vote))
+                    vote_result.result = result_value
+                    vote_result.save()
+                except:
+                    vote_result = m.Result(result=result_value, vote=vote, deputy=deputy)
+                    vote_result.save()
 
         if (not should_pass):
             continue
