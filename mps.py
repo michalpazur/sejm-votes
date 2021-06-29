3e69559522dce255b9ac52ba1d26d3fbe38fcece
michalpazur <michal.pazur1@gmail.com>
2021-06-29 16:13:02 +0200

    Deputies are saved to database

--- mps.py
+++ mps.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+from backend.models import Deputy, database
 
 letters = ["A", "B"]
 all_mps = {}
@@ -17,6 +18,26 @@ for page_letter in letters:
       party = mp.find("div", "deputy-box-details").find("strong").text
       print(name, party)
       all_mps[name] = party
+      
+      first_name = ""
+      last_name = ""
+      name = name.split(" ")
+      if ("vel" in name):
+        #👀 at you Szymon Szynkowski vel Sęk
+        first_name = " ".join(name[-1:])
+        last_name = " ".join(name[0:3])
+      else:
+        first_name = " ".join(name[1:])
+        last_name = name[0]
+
+      try:
+        deputy = Deputy.select().where((Deputy.first_name==first_name) & (Deputy.last_name==last_name)).get()
+        deputy.party = party
+        deputy.save()
+      except:
+        print("Creating deputy {} {}".format(first_name, last_name))
+        deputy = Deputy(first_name=first_name, last_name=last_name, party=party)
+        deputy.save()
 
 with open('mps.json', 'w', encoding='utf-8') as f:
   json.dump(all_mps, f, ensure_ascii=False, sort_keys=True, indent=2)
